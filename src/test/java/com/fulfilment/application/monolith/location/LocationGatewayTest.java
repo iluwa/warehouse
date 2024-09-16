043a119774458fd8b74eb356345d3483b1568050
Ilia Koliaskin <kolyaskin.ilya@gmail.com>
2024-09-16 08:27:11 +0200

    implement LocationGateway.resolveByIdentifier

--- src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
+++ src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
@@ -1,18 +1,35 @@
 package com.fulfilment.application.monolith.location;
 
+import com.fulfilment.application.monolith.warehouses.domain.models.Location;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LocationGatewayTest {
 
   @Test
   public void testWhenResolveExistingLocationShouldReturn() {
     // given
-    // LocationGateway locationGateway = new LocationGateway();
+    LocationGateway locationGateway = new LocationGateway();
 
     // when
-    // Location location = locationGateway.resolveByIdentifier("ZWOLLE-001");
+    Location location = locationGateway.resolveByIdentifier("ZWOLLE-001").get();
 
     // then
-    // assertEquals(location.identification, "ZWOLLE-001");
+    assertEquals("ZWOLLE-001", location.identification);
+  }
+
+  @Test
+  public void testWhenResolveNotExistingLocationShouldReturnNull() {
+    // given
+    LocationGateway locationGateway = new LocationGateway();
+
+    // when
+    Optional<Location> location = locationGateway.resolveByIdentifier("non-existing-location");
+
+    // then
+    assertTrue(location.isEmpty());
   }
 }
